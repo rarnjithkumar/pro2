2bd26a32c1a71193d5b8406cd62718df51d50d09
rarnjithkumar <53113753+rarnjithkumar@users.noreply.github.com>
2019-07-20 09:01:34 -0800

    Update vowel or consonant.py

--- vowel or consonant.py
+++ vowel or consonant.py
@@ -2,6 +2,6 @@ r=input()
 if(r=='a' or r=='e' or r=='i' or r=='o' or r=='u' or r=='A' or r=='E' or r=='I' or r=='O' or r=='U'):
   print("Vowel")
 elif(r>='a'and r<='z' or r>='A' and r<='Z'):
-  print("consonant")
+  print("Consonant")
 else:
   print("invalid")
