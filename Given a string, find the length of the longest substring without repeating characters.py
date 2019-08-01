b5fde4f71943842339337a1b1ddd433d8e4e3b3c
rarnjithkumar <53113753+rarnjithkumar@users.noreply.github.com>
2019-08-01 15:04:42 +0530

    Create Given a string, find the length of the longest substring without repeating characters.py

---
+++ Given a string, find the length of the longest substring without repeating characters.py
@@ -0,0 +1,17 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        longest, start, visited = 0, 0, [False for _ in range(256)]
+        for i, char in enumerate(s):
+            if visited[ord(char)]:
+                while char != s[start]:
+                    visited[ord(s[start])] = False
+                    start += 1
+                start += 1
+            else:
+                visited[ord(char)] = True
+            longest = max(longest, i - start + 1)
+        return longest
+
+if __name__ == "__main__":
+    s=input()
+    print (Solution().lengthOfLongestSubstring(s))
